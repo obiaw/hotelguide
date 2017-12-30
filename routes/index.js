a27c41f7e91ee58b6e5da5d607b72bebfa439a23
=obia.williams <=obia.williams@gmail.com>
2017-12-30 13:57:35 +0300

    email fixing v20

--- routes/index.js
+++ routes/index.js
@@ -75,16 +75,61 @@ router.post('/addcustomer', function(req, res) {
           else {
               console.log(customers);
               data = {'message': 'Booking successful!'};
+              sendEmail(req,res);
               return res.send(data.message);
-              
-            let transporter = nodemailer.createTransport({
-                sendmail: true,
-                newline: 'unix',
-                // path: '/usr/sbin/sendmail'
-            });
+
+            // let transporter = nodemailer.createTransport({
+            //     sendmail: true,
+            //     newline: 'unix',
+            //     // path: '/usr/sbin/sendmail'
+            // });
             
-           var mailoutput = "<html><body>"+
-              "<p>Hello Sheraton Hotel, you have a booking from "+
+           // var mailoutput = "<html><body>"+
+           //    "<p>Hello Sheraton Hotel, you have a booking from "+
+           //    req.body.fullname+". Find full details below; </p><br>"+
+           //    "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
+           //    "<b>Email</b>: "+req.body.email+ "<br>"+
+           //    "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
+           //    "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
+           //    "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
+           //    "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
+           //    "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
+           //    "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
+           //    "<p>You can confirm this booking by calling the customer "+
+           //    "or sending them an email. "+
+           //    "Thank you for using Hotel Guide as your booking agent. </p>"+
+           //    "</body></html>";
+
+               // transporter.sendMail({
+               //      from: 'obia@hivetechug.com',
+               //      to: 'rowlandsemmy@gmail.com',
+               //      cc: 'obia.williams@gmail.com',
+               //      bcc: 'kgidion1@gmail.com',
+               //      subject: 'Hotel Booking',
+               //      html: mailoutput,
+               // });
+          }
+      });
+    }
+  });
+});
+
+
+function sendEmail(req,res){
+
+  let transporter = nodemailer.createTransport({
+    sendmail:true,
+    newline:'unix',
+
+});
+
+  transporter.sendMail({
+    from: req.body.email,
+    to  : 'obia.williams@gmail.com',
+    cc  : 'obia@hivetechug.com',
+    bcc :'kgidion1@gmail.com',
+    subject: "Hotel Booking",
+    html: "<p>Hello Sheraton Hotel, you have a booking from "+
               req.body.fullname+". Find full details below; </p><br>"+
               "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
               "<b>Email</b>: "+req.body.email+ "<br>"+
@@ -96,21 +141,12 @@ router.post('/addcustomer', function(req, res) {
               "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
               "<p>You can confirm this booking by calling the customer "+
               "or sending them an email. "+
-              "Thank you for using Hotel Guide as your booking agent. </p>"+
-              "</body></html>";
+              "Thank you for using Hotel Guide as your booking agent. </p>"},
+    (err,info)=>{
+      console.log(info);
+      console.log(err);
+    });
 
-               transporter.sendMail({
-                    from: 'obia@hivetechug.com',
-                    to: 'rowlandsemmy@gmail.com',
-                    cc: 'obia.williams@gmail.com',
-                    bcc: 'kgidion1@gmail.com',
-                    subject: 'Hotel Booking',
-                    html: mailoutput,
-               });
-          }
-      });
-    }
-  });
-});
+}
 
 module.exports = router;
