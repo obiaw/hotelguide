39c929a2f97fae77dc17b062ebb25866061b70b7
kgidion1 <kgidion1@gmail.com>
2017-12-30 20:19:25 +0300

    changes on hotelguide on 30122017

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HotelGuide Bot'});
 });
 
+function sendEmail(req,res){
+
+    let transporter = nodemailer.createTransport({
+        sendmail:true,
+        newline:'unix',
+
+    });
+
+    transporter.sendMail({
+            from: req.body.email,
+            to  : 'obia.williams@gmail.com',
+            cc  : 'obia@hivetechug.com',
+            bcc :'kgidion1@gmail.com',
+            subject: "Hotel Booking",
+            html: "<p>Hello Sheraton Hotel, you have a booking from "+
+            req.body.fullname+". Find full details below; </p><br>"+
+            "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
+            "<b>Email</b>: "+req.body.email+ "<br>"+
+            "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
+            "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
+            "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
+            "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
+            "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
+            "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
+            "<p>You can confirm this booking by calling the customer "+
+            "or sending them an email. "+
+            "Thank you for using Hotel Guide as your booking agent. </p>"},
+        (err,info)=>{
+        console.log(info);
+    console.log(err);
+});
+
+}
+
 /* POST to Add User Service */
 router.post('/addcustomer', function(req, res) {
     // Set our internal DB variable
@@ -73,40 +107,41 @@ router.post('/addcustomer', function(req, res) {
             // console.log(err.message);
           }
           else {
-              console.log(customers);
+              // console.log(customers);
+              sendEmail(req,res)
               data = {'message': 'Booking successful!'};
               return res.send(data.message);
 
-            let transporter = nodemailer.createTransport({
-                sendmail: true,
-                newline: 'unix',
-                path: '/usr/sbin/sendmail'
-            });
-            
-           var mailoutput = "<html><body>"+
-              "<p>Hello Sheraton Hotel, you have a booking from "+
-              req.body.fullname+". Find full details below; </p><br>"+
-              "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
-              "<b>Email</b>: "+req.body.email+ "<br>"+
-              "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
-              "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
-              "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
-              "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
-              "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
-              "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
-              "<p>You can confirm this booking by calling the customer "+
-              "or sending them an email. "+
-              "Thank you for using Hotel Guide as your booking agent. </p>"+
-              "</body></html>";
-
-               transporter.sendMail({
-                    from: 'obia@hivetechug.com',
-                    to: 'rowlandsemmy@gmail.com',
-                    cc: 'obia.williams@gmail.com',
-                    bcc: 'kgidion1@gmail.com',
-                    subject: 'Hotel Booking',
-                    html: mailoutput
-               });
+           //  var transporter = nodemailer.createTransport({
+           //      sendmail: true,
+           //      newline: 'unix',
+           //      path: '/usr/sbin/sendmail'
+           //  });
+           //
+           // var mailoutput = "<html><body>"+
+           //    "<p>Hello Sheraton Hotel, you have a booking from "+
+           //    req.body.fullname+". Find full details below; </p><br>"+
+           //    "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
+           //    "<b>Email</b>: "+req.body.email+ "<br>"+
+           //    "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
+           //    "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
+           //    "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
+           //    "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
+           //    "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
+           //    "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
+           //    "<p>You can confirm this booking by calling the customer "+
+           //    "or sending them an email. "+
+           //    "Thank you for using Hotel Guide as your booking agent. </p>"+
+           //    "</body></html>";
+           //
+           //     transporter.sendMail({
+           //          from: 'obia@hivetechug.com',
+           //          to: 'rowlandsemmy@gmail.com',
+           //          cc: 'obia.williams@gmail.com',
+           //          bcc: 'kgidion1@gmail.com',
+           //          subject: 'Hotel Booking',
+           //          html: mailoutput
+           //     });
           }
       });
     }
