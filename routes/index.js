581f936435a1d71201fc3ef5e0423798148e4c8c
=obia.williams <obia.williams@gmail.com>
2017-12-30 20:52:48 +0300

    Merger success

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HotelGuide Bot'});
 });
 
+
 function sendEmail(req,res){
 
     let transporter = nodemailer.createTransport({
@@ -113,99 +114,16 @@ router.post('/addcustomer', function(req, res) {
               sendEmail(req,res);
               return res.send(data.message);
 
-<<<<<<< HEAD
-           //  var transporter = nodemailer.createTransport({
-           //      sendmail: true,
-           //      newline: 'unix',
-           //      path: '/usr/sbin/sendmail'
-           //  });
-           //
-=======
-            // let transporter = nodemailer.createTransport({
-            //     sendmail: true,
-            //     newline: 'unix',
-            //     // path: '/usr/sbin/sendmail'
-            // });
-            
->>>>>>> master
-           // var mailoutput = "<html><body>"+
-           //    "<p>Hello Sheraton Hotel, you have a booking from "+
-           //    req.body.fullname+". Find full details below; </p><br>"+
-           //    "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
-           //    "<b>Email</b>: "+req.body.email+ "<br>"+
-           //    "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
-           //    "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
-           //    "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
-           //    "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
-           //    "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
-           //    "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
-           //    "<p>You can confirm this booking by calling the customer "+
-           //    "or sending them an email. "+
-           //    "Thank you for using Hotel Guide as your booking agent. </p>"+
-           //    "</body></html>";
-<<<<<<< HEAD
-           //
-           //     transporter.sendMail({
-           //          from: 'obia@hivetechug.com',
-           //          to: 'rowlandsemmy@gmail.com',
-           //          cc: 'obia.williams@gmail.com',
-           //          bcc: 'kgidion1@gmail.com',
-           //          subject: 'Hotel Booking',
-           //          html: mailoutput
-           //     });
-=======
 
-               // transporter.sendMail({
-               //      from: 'obia@hivetechug.com',
-               //      to: 'rowlandsemmy@gmail.com',
-               //      cc: 'obia.williams@gmail.com',
-               //      bcc: 'kgidion1@gmail.com',
-               //      subject: 'Hotel Booking',
-               //      html: mailoutput,
-               // });
->>>>>>> master
-          }
+      } 
+
       });
-    }
-  });
-});
-<<<<<<< HEAD
-=======
 
-
-function sendEmail(req,res){
-
-  let transporter = nodemailer.createTransport({
-    sendmail:true,
-    newline:'unix',
-
-});
-
-  transporter.sendMail({
-    from: req.body.email,
-    to  : 'obia.williams@gmail.com',
-    cc  : 'obia@hivetechug.com',
-    bcc :'kgidion1@gmail.com',
-    subject: "Hotel Booking",
-    html: "<p>Hello Sheraton Hotel, you have a booking from "+
-              req.body.fullname+". Find full details below; </p><br>"+
-              "<b>Fullname:</b> "+req.body.fullname+ "<br>"+
-              "<b>Email</b>: "+req.body.email+ "<br>"+
-              "<b>Phonenumber</b>: "+req.body.phonenumber+ "<br>"+
-              "<b>Room Type</b>: "+req.body.rooms+ "<br>"+
-              "<b>Total Amount</b>: "+req.body.amount+ "<br>"+
-              "<b>Check-in Date</b>: "+req.body.checkin_date+ "<br>"+
-              "<b>Check-out Date</b>: "+req.body.checkout_date+ "<br>"+
-              "<b>Payment Method</b>: "+req.body.pay+ "<br>"+
-              "<p>You can confirm this booking by calling the customer "+
-              "or sending them an email. "+
-              "Thank you for using Hotel Guide as your booking agent. </p>"},
-    (err,info)=>{
-      console.log(info);
-      console.log(err);
+        }
     });
 
-}
->>>>>>> master
+
+});
+
 
 module.exports = router;
